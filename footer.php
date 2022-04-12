d70b4d54a39addb7566fff2b05892f8508f15389
NikSilver03 <nikki@silverthorne.net.au>
2022-04-12 20:00:46 +0800

    footer widget

--- footer.php
+++ footer.php
@@ -1,9 +1,12 @@
-<footer class="container-fluid text-center"><!-- div for the main image to stretch s to the edge of the page-->
+ <footer class="container-fluid text-center"><!-- div for the main image to stretch s to the edge of the page-->
 
                 <div class="container"><!-- div to hold the content in the middle of the page-->
                     <div class="row"><!-- a row that gives us access to the BS columns-->
-                        <div class="col-md-6 ">
-                          <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange"> WHITKIN</span></a>
+                        <div class="col-md-6 widgetleft">
+                    
+                          <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
+
                         </div>
                         <div class="col-md-6 ">
                             <p>Terms and Conditions</p>
